3ae050622ee59ab92e32a9961c8ee739daed70c2
Wes George <wesley.george@daylight.io>
2022-07-21 11:29:42 -0400

    :sparkles: include FH domain in js output, as well as download time.

--- src/js-formatter.js
+++ src/js-formatter.js
@@ -1,16 +1,24 @@
 const beautify = require('js-beautify')
+const Handlebars = require('handlebars')
+Handlebars.registerHelper('dateFormat', require('handlebars-dateformat'));
 
-function formMetaAsHeader(form) {
-	return `/*
-* title: ${form.title}
-* organization: ${form.organization}
-* team: ${form.team}
-* slug: ${form.slug}
-*
-* version: ${form.version.label}
-* cuid: ${form.cuid}
-* created: ${form.created}
-*/`
+function formMetaAsHeader(form, domain) {
+	return Handlebars.compile([
+		"/*",
+		" * title: {{form.title}}",
+		" * organization: {{form.organization}}",
+		" * team: {{form.team}}",
+		" * slug: {{form.slug}}",
+		" *{{#if domain}} domain: {{ domain }}\n *{{/if}}",
+
+		" * version: {{form.version.label}}",
+		" * cuid: {{form.cuid}}",
+		" * created: {{form.created}}",
+		" *",
+		" * downloaded at: {{ dateFormat now ''}}",
+		" */",
+		].join('\n')
+	)({ form, domain, now: Date.now() })
 }
 
 function globalJsAsString(form) {
@@ -28,9 +36,9 @@ label: "${node.nodeLabel}",
 ${js}
 })`}
 
-function fullFormAsString(form) {
+function fullFormAsString(form, domain) {
 	return [
-		formMetaAsHeader(form),
+		formMetaAsHeader(form, domain),
 		globalJsAsString(form),
 		`const nodes = [];`,
 		...(form.decisionTree.nodes.filter(n => n.useOnChangeJavaScript).map(nodeJsAsString)),
